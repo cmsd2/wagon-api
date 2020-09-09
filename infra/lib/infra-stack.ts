b61d10ec96b2a4a999a52726785c931efa7029fd
Chris Dawes <cmsd2@cantab.net>
2020-09-09 19:22:20 +0100

    deploy lambda

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,31 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as iam from "@aws-cdk/aws-iam";
+import * as path from "path";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lambdaRole = new iam.Role(this, "api", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+
+    new lambda.Function(this, "api", {
+      runtime: lambda.Runtime.PROVIDED,
+      handler: "unused",
+      code: lambda.Code.fromAsset(
+        path.join("..", "target", "release", "api.zip")
+      ),
+      memorySize: 128,
+      role: lambdaRole,
+      timeout: cdk.Duration.seconds(2),
+    });
   }
 }
