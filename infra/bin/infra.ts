7630ceeb468fac70b98401d8ba61f002b1877ac6
Chris Dawes <cmsd2@cantab.net>
2020-09-09 20:23:28 +0100

    fix import paths

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { WagonApiStack } from "../lib/infra-stack";
+import { WagonApiStack } from "../lib/wagon-api-stack";
 
 const app = new cdk.App();
 new WagonApiStack(app, "WagonApi");
--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Infra from '../lib/infra-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+  haveResource,
+} from "@aws-cdk/assert";
+import "@aws-cdk/assert/jest";
+import * as cdk from "@aws-cdk/core";
+import * as Infra from "../lib/wagon-api-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Infra.InfraStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new Infra.WagonApiStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::IAM::Role", {}));
+  expectCDK(stack).to(haveResource("AWS::Lambda::Function", {}));
 });
